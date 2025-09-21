28f14e1b7dc0e86d48d552b314218641d2f04947
Terranul <163475387+Terranul@users.noreply.github.com>
2025-09-20 17:06:34 -0700

    Create Plane.swift

---
+++ Plane.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct PlaneView: View {
+    
+    // set Content in the middle of the screen
+    @State var initialPosition: CGPoint = CGPoint(x: UIScreen.main.bounds.width/2, y: UIScreen.main.bounds.height/2)
+    @State var offsetValue: CGSize = .zero
+    
+    var body: some View {
+        ZStack {
+            // create transparent shape so entire screen is draggable
+            Color.clear.contentShape(Rectangle())
+            PlaneContentView()
+                .position(initialPosition)
+                .offset(x: offsetValue.width, y: offsetValue.height)
+            
+        }
+        .ignoresSafeArea()
+        .gesture(
+            DragGesture()
+                .onChanged {value in
+                    offsetValue = value.translation
+                }
+                .onEnded {value in
+                    // update position but snap back into valid bounds
+                        // update position normally
+                        initialPosition = CGPoint(x: initialPosition.x + value.translation.width, y: initialPosition.y + value.translation.height)
+                        offsetValue = .zero
+                }
+        )
+    }
+}
